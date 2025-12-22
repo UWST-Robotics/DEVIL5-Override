4d3920eae9a8342a9ccdd5e1dfca4d0d016c1745
Iwild29 <iawild29@gmail.com>
2025-12-22 10:38:31 -0600

    Robin Code!
    Dearest Austin (Digi),
    
    I have taken some time to add the code which I require to Robin in order to control the new stick bot which I have constructed. Interestingly enough I found that this code actually worked as intended first try (a miracle, I know). I hope you enjoy the code which I have created for Robin (I will be adding more soon).
    
    Enjoy your holidays,
    Isaac Wild

--- include/devils/2026/robinRobot.hpp
+++ include/devils/2026/robinRobot.hpp
@@ -27,6 +27,42 @@ namespace devils
 
         void opcontrol() override
         {
+            while (true)
+            {
+                // Take Controller Inputs
+                double leftY = mainController.get_analog(ANALOG_LEFT_Y) / 127.0;
+                double leftX = mainController.get_analog(ANALOG_LEFT_X) / 127.0;
+                double rightX = mainController.get_analog(ANALOG_RIGHT_X) / 127.0;
+                double rightY = mainController.get_analog(ANALOG_RIGHT_Y) / 127.0;
+
+                //stick movement buttons, will replace later with macros to move stick to preset rotations
+                bool stickScoreButton = mainController.get_digital(DIGITAL_R1); //temp button to move stick
+                bool stickResetButton = mainController.get_digital(DIGITAL_R2); //temp button to move stick
+
+                if (stickScoreButton) stick.moveVoltage(0.7f);
+                else if (stickResetButton) stick.moveVoltage(-0.5f);
+                else stick.moveVoltage(0.0f);
+
+                floatingRollers.moveVoltage(rightY);
+
+                // Curve Joystick Inputs
+                leftY = JoystickCurve::curve(leftY, 3.0, 0.1, 0.15);
+                leftX = JoystickCurve::curve(leftX, 3.0, 0.05, 0.2);
+                rightX = JoystickCurve::curve(rightX, 3.0, 0.1, 0.2);
+                rightY = JoystickCurve::curve(rightY, 3.0, 0.1, 0.15);
+
+                rightX *= 0.5;
+
+                // Combine Left and Right X Joystick Inputs
+                double combinedX = JoystickCurve::combine(leftX, rightX);
+
+                // Drive normally
+                chassis.move(leftY, combinedX * 0.5f);
+
+                // Delay to prevent the CPU from being overloaded
+                pros::delay(20);
+            }
+            
         }
 
         void disabled() override
@@ -37,9 +73,11 @@ namespace devils
         // VEXBridge bridge = VEXBridge();
 
         // Hardware
-        SmartMotorGroup leftMotors = SmartMotorGroup("LeftMotors", {-6, 7, -8, 9, -10});
-        SmartMotorGroup rightMotors = SmartMotorGroup("RightMotors", {16, -17, 18, -19, 20});
-        InertialSensor imu = InertialSensor("IMU", 15);
+        SmartMotorGroup leftMotors = SmartMotorGroup("LeftMotors", {16, -17, 18, -19, 20});
+        SmartMotorGroup rightMotors = SmartMotorGroup("RightMotors", {-11, 12, -13, 14, -15});
+        SmartMotorGroup floatingRollers = SmartMotorGroup("FloatingRoller", {-6});
+        SmartMotorGroup stick = SmartMotorGroup("Stick", {-4});
+        InertialSensor imu = InertialSensor("IMU", 1);
 
         // Subsystems
         TankChassis chassis = TankChassis(leftMotors, rightMotors);
